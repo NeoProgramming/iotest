ea426e6998feba79f68e97fc2e79e0871cf3b573
NeoProgramming <neoprogramming@gmail.com>
2022-05-16 19:14:53 +0300

    include

---
+++ book.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// Fix back button cache problem
+window.onunload = function () { };
+
+
+(function sidebar() {
+ //   var html = document.querySelector("html");
+    var sidebar = document.getElementById("sidebar");
+    var sidebarLinks = document.querySelectorAll('#sidebar a');
+    var sidebarResizeHandle = document.getElementById("sidebar-resize-handle");
+    var firstContact = null;
+	
+	var recentOffsetX = localStorage.getItem('recentSidebarPos');
+	document.documentElement.style.setProperty('--sidebar-width', recentOffsetX);
+	console.log("function sidebar() start: ", recentOffsetX)
+
+    sidebarResizeHandle.addEventListener('mousedown', initResize, false);
+
+    function initResize(e) {
+        window.addEventListener('mousemove', resize, false);
+        window.addEventListener('mouseup', stopResize, false);
+   //     html.classList.add('sidebar-resizing');
+    }
+	
+	// visual resize
+    function resize(e) {
+        var pos = (e.clientX - sidebar.offsetLeft);
+
+		pos = Math.min(pos, window.innerWidth - 100);
+        document.documentElement.style.setProperty('--sidebar-width', pos + 'px');
+    }
+	
+    // on mouseup remove windows functions mousemove & mouseup
+    function stopResize(e) {
+   //     html.classList.remove('sidebar-resizing');
+        window.removeEventListener('mousemove', resize, false);
+        window.removeEventListener('mouseup', stopResize, false);
+		
+		var pos = (e.clientX - sidebar.offsetLeft);
+		
+		console.log("stopResize: ", pos)
+		localStorage.setItem('recentSidebarPos', pos);
+    }
+
+    // Scroll sidebar to current active section
+    var activeSection = document.getElementById("sidebar").querySelector(".active");
+    if (activeSection) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+        activeSection.scrollIntoView({ block: 'center' });
+    }
+})();
+
